4e6ea0462d2b8e3fc825fd0611b96ce5bf842cb8
Diogo Mendes <diogo.santos.mendes@hotmail.com>
2018-05-21 23:08:54 +0100

    Update main.js

--- border-router/main.js
+++ border-router/main.js
@@ -5,9 +5,16 @@ coap.registerFormat('application/json', 50);
 var express = require('express');
 var app = express();
 
-app.get('/dht22', function (req, res) {
-  var coap_req = coap.request({
-    hostname: 'fd00::212:4b00:14d5:2bf9',
+var zolertias = [
+  { "id": 1, "ip": "fd00::212:4b00:14d5:2bf9", location: "salão" }
+]
+
+app.get('/:id', function (req, res) {
+  let values = {}, counter = 0;
+  values.locat = zolertias[req.params.id].location;
+
+  var dht22 = coap.request({
+    hostname: zolertias[req.params.id].ip,
     port: 5683,
     method: "GET",
     pathname: "/sensors/dht22",
@@ -16,23 +23,12 @@ app.get('/dht22', function (req, res) {
   	PSKIdent:      new Buffer("OurIdentity")
     }*/
   });
+  dht22.on('response', (coap_res) => { values.temp = JSON.stringify(coap_res._packet.payload.toString('utf8')).temp; values.hum = JSON.stringify(coap_res._packet.payload.toString('utf8')).hum; counter += 1; });
+  dht22.on('error', (coap_res) => { values.temp = null; values.humi = null; counter += 1; });
+  dht22.end();
 
-  coap_req.on('response', (coap_res) => {
-    console.log(coap_res._packet.payload.toString('utf8'));
-    res.send(coap_res._packet.payload.toString('utf8'));
-  });
-
-  coap_req.on('error', (coap_res) => {
-    console.log(coap_res);
-    res.send("error");
-  });
-
-  coap_req.end()
-})
-
-app.get('/loudness', function (req, res) {
-  var coap_req = coap.request({
-    hostname: 'fd00::212:4b00:14d5:2bf9',
+  var loud = coap.request({
+    hostname: zolertias[req.params.id].ip,
     port: 5683,
     method: "GET",
     pathname: "/sensors/loudness",
@@ -41,23 +37,12 @@ app.get('/loudness', function (req, res) {
   	PSKIdent:      new Buffer("OurIdentity")
     }*/
   });
+  loud.on('response', (coap_res) => { values.loud = JSON.stringify(coap_res._packet.payload.toString('utf8')).loudness; counter += 1; });
+  loud.on('error', (coap_res) => { values.loud = null; counter += 1; });
+  loud.end();
 
-  coap_req.on('response', (coap_res) => {
-    console.log(coap_res._packet.payload.toString('utf8'));
-    res.send(coap_res._packet.payload.toString('utf8'));
-  });
-
-  coap_req.on('error', (coap_res) => {
-    console.log(coap_res);
-    res.send("error");
-  });
-
-  coap_req.end()
-})
-
-app.get('/light', function (req, res) {
-  var coap_req = coap.request({
-    hostname: 'fd00::212:4b00:14d5:2bf9',
+  var light = coap.request({
+    hostname: zolertias[req.params.id].ip,
     port: 5683,
     method: "GET",
     pathname: "/sensors/light-sensor",
@@ -66,16 +51,15 @@ app.get('/light', function (req, res) {
   	PSKIdent:      new Buffer("OurIdentity")
     }*/
   });
+  light.on('response', (coap_res) => { values.light = JSON.stringify(coap_res._packet.payload.toString('utf8')).light; counter += 1; });
+  light.on('error', (coap_res) => { values.light = null; counter += 1; });
+  light.end();
 
-  coap_req.on('response', (coap_res) => {
-    console.log(coap_res._packet.payload.toString('utf8'));
-    res.send(coap_res._packet.payload.toString('utf8'));
-  });
+  function waiting() {
+    if (counter < 3) setTimeout(waiting, 1000);
+    else res.send(values);
+  }
 
-  coap_req.on('error', (coap_res) => {
-    console.log(coap_res);
-    res.send("error");
-  });
+});
 
-  coap_req.end()
-})
+app.listen(3000, () => { console.log("server running"); });
