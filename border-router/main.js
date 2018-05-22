2bcb7cc4ef1eb68a783a76988af2a983c92050d1
Rafael Escudeiro <rafael-tome@hotmail.com>
2018-05-22 21:46:28 +0000

    Add files via upload

--- border-router/main.js
+++ border-router/main.js
@@ -1,64 +1,83 @@
-var coap = require('coap');
-
-coap.registerFormat('application/json', 50);
-
 var express = require('express');
 var app = express();
+var coap = require('coap');
+coap.registerFormat('application/json', 50);
 
 var zolertias = [
-  { "id": 1, "ip": "fd00::212:4b00:14d5:2bf9", location: "salão" }
+  { id: 1, ip: "fd00::212:4b00:14d5:2bf9", location: "salão" }
 ]
 
-app.get('/:id', function (req, res) {
-  let values = {}, counter = 0;
-  values.locat = zolertias[req.params.id].location;
+app.get('/:id',  function (req, res) {
 
-  var dht22 = coap.request({
-    hostname: zolertias[req.params.id].ip,
-    port: 5683,
-    method: "GET",
-    pathname: "/sensors/dht22",
-    /* options: {
-  	psk:           new Buffer('OurSecret'),
-  	PSKIdent:      new Buffer("OurIdentity")
-    }*/
-  });
-  dht22.on('response', (coap_res) => { values.temp = JSON.stringify(coap_res._packet.payload.toString('utf8')).temp; values.hum = JSON.stringify(coap_res._packet.payload.toString('utf8')).hum; counter += 1; });
-  dht22.on('error', (coap_res) => { values.temp = null; values.humi = null; counter += 1; });
-  dht22.end();
+  let zolertia = zolertias[req.params.id - 1];
 
-  var loud = coap.request({
-    hostname: zolertias[req.params.id].ip,
-    port: 5683,
-    method: "GET",
-    pathname: "/sensors/loudness",
-    /* options: {
-  	psk:           new Buffer('OurSecret'),
-  	PSKIdent:      new Buffer("OurIdentity")
-    }*/
-  });
-  loud.on('response', (coap_res) => { values.loud = JSON.stringify(coap_res._packet.payload.toString('utf8')).loudness; counter += 1; });
-  loud.on('error', (coap_res) => { values.loud = null; counter += 1; });
-  loud.end();
+  console.log(zolertia);
 
-  var light = coap.request({
-    hostname: zolertias[req.params.id].ip,
-    port: 5683,
-    method: "GET",
-    pathname: "/sensors/light-sensor",
-    /* options: {
-  	psk:           new Buffer('OurSecret'),
-  	PSKIdent:      new Buffer("OurIdentity")
-    }*/
-  });
-  light.on('response', (coap_res) => { values.light = JSON.stringify(coap_res._packet.payload.toString('utf8')).light; counter += 1; });
-  light.on('error', (coap_res) => { values.light = null; counter += 1; });
-  light.end();
+  let getDHT22 = new Promise( (resolve, reject) => {
+      coap.request({
+        hostname: zolertia.ip,
+        port: 5683,
+        method: "GET",
+        pathname: "/sensors/dht22",
+        // options: {
+        //   psk:           new Buffer('OurSecret'),
+        //   PSKIdent:      new Buffer("OurIdentity")
+        // }
+      })
+      .on('response', (coap_res) => { 
+        let response=coap_res._packet.payload.toString('utf8');
+        if(response.length>0) resolve(JSON.parse(JSON.stringify(eval("("+coap_res._packet.payload.toString('utf8')+")"))));
+        else resolve({ temp: null,  hum: null });
+      })
+      .on('error', (coap_res) => reject({ temp: null,  hum: null }))
+      .end();
+  })
+  let getLoud = new Promise( (resolve, reject) => {
+      coap.request({
+        hostname: zolertia.ip,
+        port: 5683,
+        method: "GET",
+        pathname: "/sensors/loudness",
+        // options: {
+        //   psk:           new Buffer('OurSecret'),
+        //   PSKIdent:      new Buffer("OurIdentity")
+        // }
+      })
+      .on('response', (coap_res) => { 
+        let response=coap_res._packet.payload.toString('utf8');
+        if(response.length>0) resolve(JSON.parse(JSON.stringify(eval("("+coap_res._packet.payload.toString('utf8')+")"))));
+        else resolve({ loudness: null });
+      })
+      .on('error', (coap_res) => reject({ loudness: null }))
+      .end();
+  })
+  let getLight = new Promise( (resolve, reject) => {
+      coap.request({
+        hostname: zolertia.ip,
+        port: 5683,
+        method: "GET",
+        pathname: "/sensors/light-sensor",
+        // options: {
+        //   psk:           new Buffer('OurSecret'),
+        //   PSKIdent:      new Buffer("OurIdentity")
+        // }
+      })
+      .on('response', (coap_res) => { 
+        let response=coap_res._packet.payload.toString('utf8');
+        if(response.length>0) resolve(JSON.parse(JSON.stringify(eval("("+coap_res._packet.payload.toString('utf8')+")"))));
+        else resolve({ light: null });
+      })
+      .on('error', (coap_res) => reject({ light: null }))
+      .end();
+  })
 
-  function waiting() {
-    if (counter < 3) setTimeout(waiting, 1000);
-    else res.send(values);
-  }
+  Promise.all([getDHT22, getLight, getLoud]).then(
+    values=>{
+      console.log(values);
+      res.send(Object.assign({}, {locat: zolertia.location}, ...values));
+    },
+    error=> res.send("ERROR")
+  );
 
 });
 
