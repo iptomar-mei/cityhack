45068e6365764a3e894dc66236d879fc31c02c6f
Xavier <bxavier92@gmail.com>
2018-07-06 17:22:28 +0100

    Contiki Files COAP

---
+++ contiki-ng/examples/coap/coap-example-server/obj_zoul/heapmem.d
@@ -0,0 +1,6 @@
+obj_zoul/heapmem.o: ../../../os/lib/heapmem.c \
+ ../../../os/net/app-layer/coap/module-macros.h project-conf.h \
+ ../../../os/lib/heapmem.h
+../../../os/lib/heapmem.c :
+ ../../../os/net/app-layer/coap/module-macros.h project-conf.h :
+ ../../../os/lib/heapmem.h :
---
+++ contiki-ng/examples/coap/coap-example-server/obj_zoul/syscalls.d
@@ -0,0 +1,4 @@
+obj_zoul/syscalls.o: ../../../os/lib/newlib/syscalls.c \
+ ../../../os/net/app-layer/coap/module-macros.h
+../../../os/lib/newlib/syscalls.c :
+ ../../../os/net/app-layer/coap/module-macros.h :
